8387e43af62bded6e0f8fd77a01166288181f682
LINCOLNmgc <lin.lincoln@hotmail.com>
2022-04-12 21:18:41 -0300

    exercicio sobre aula de variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,73 @@
+1
+/*
+
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+
+Q1 - indica que a 
+primeira saida (a) sera 10
+segunda saida (b) sera 10
+e para "console.log(b)" a saida sera 10,5
+
+
+2
+
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+
+Q2 - as saidas sao 10, 10, 10
+por conta da variavel 
+
+
+
+3
+
+
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+sugestoes (p) valorHoraTrabalhada /// (t)valorDia
+
+
+*/
+
+
+
+const nome =prompt("qual seu nome")
+const idade =prompt("qual sua idade")
+
+typeof nome
+typeof idade
+
+prompt("Qual seu nome?")
+prompt("Qual sua idade?")
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+
+
+/* resposta - indefinido, nenhum valor 
+foi declarado para nome, apenas variavel */
+
+
+
+
+
+
+
+
+
