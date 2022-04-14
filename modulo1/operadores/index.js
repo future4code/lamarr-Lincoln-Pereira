ea73bba7f64e4a9d4111b12bd976689e7311da4d
LINCOLNmgc <lin.lincoln@hotmail.com>
2022-04-14 18:59:55 -0300

    exercicios aula operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,118 @@
+/*
+
+const bool1 = true
+const bool2 = false 
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado) // false
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) // false
+              
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado)  // true
+
+console.log("d. ", typeof resultado) // boolean, por conta da condicao 
+
+*/
+/*
+
+let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = primeiroNumero + segundoNumero 
+
+// ele entende que o tipo, é string... por isso ele nao soma 20+15 e da o resultado de 35
+// ele concatena e mostra 2015 (juntando)
+
+console.log(soma) 
+*/
+/*
+let primeiroNumero = 20
+let segundoNumero = 15
+
+primeiroNumero = prompt("Digite um numero!")
+segundoNumero = prompt("Digite outro numero!")
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)
+
+*/
+
+
+/*
+
+let primeiroNumero = Number(prompt("Digite um numero!"))
+let segundoNumero = Number(prompt("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)
+
+*/
+
+/*
+
+let idade = Number(prompt("Qual e sua idade?"))
+let amigoIdade = Number(prompt("Qual a idade do seu melhor amigo"))
+
+const comparacao = idade > amigoIdade
+console.log("Sua idade é maior do que a do seu melhor amigo?", comparacao)
+
+*/
+
+/*
+
+let numeroPar = Number(prompt("insira um numero par"))
+console.log( numeroPar % 2)
+
+const sobra = numeroPar % 2 == 0
+console.log(sobra)
+ // se o usuario inserir um numero par, sempre resultara em 0
+ // se o usuario inserir um numero impar, sempre resultara em sobra 1
+
+ 
+*/
+
+/*
+
+let idadeAnos = Number(prompt("qual eh a sua idade?"))
+console.log("IDADE:", idadeAnos)
+
+let idadeMes = idadeAnos * 12
+console.log("IDADE EM MESES:", idadeMes)
+
+let idadeDias = idadeAnos * 365
+console.log("IDADE EM DIAS:", idadeDias)
+
+let idadeHoras = idadeDias * 24 
+console.log("IDADE EM HORAS:", idadeHoras)
+
+*/
+
+/*
+
+
+let num1 = Number(prompt("Insira um numero"))
+let num2 = Number(prompt("Insira outro numero"))
+
+let comparacao1 = num1 > num2
+console.log("O primeiro numero é maior que segundo?", comparacao1)
+
+comparacao1 = num1 == num2
+console.log("O primeiro numero é igual ao segundo?", comparacao1)
+
+comparacao1 = num1 % num2 === 0
+console.log ("O primeiro numero é divisível pelo segundo?", comparacao1)
+
+comparacao1 = num2 % num1 === 0
+console.log ("O segundo numero é divisível pelo primeiro?", comparacao1)
+
+
+*/
+
+
+
+
