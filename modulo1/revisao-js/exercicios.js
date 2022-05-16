e71106d30a97c777b7b87c9ba0fa9ec71d02964f
LINCOLNmgc <lin.lincoln@hotmail.com>
2022-05-15 22:12:53 -0300

    ultima atualizacao

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -51,6 +51,13 @@ function retornaMaiorNumero(array) {
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
 
+    const objeto = {
+        maiorNumero: Math.max(num1,num2),
+        maiorDivisivelPorMenor: Math.max(num1,num2)%Math.min (num1,num2)===0,
+        diferenca: Math.max(num1,num2)- Math.min (num1,num2),
+    }
+       return objeto
+
 }
 
 // EXERCÍCIO 08
@@ -118,10 +125,28 @@ function retornaPessoasAutorizadas(pessoas) {
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
   
+    let naoAutorizadas = [];
+    for (item of pessoas) {
+        if (item.altura < 1.5 || item.idade <= 14 || item.idade > 60) {
+            naoAutorizadas.push(item);
+    }}
+
+    return naoAutorizadas;
+
 }
 
 // EXERCÍCIO 14
 function retornaContasComSaldoAtualizado(contas) {
+
+    for (item of contas) {
+        let dividaTotal = 0;
+        for (devedor of item.compras) {
+            dividaTotal += devedor;
+    }
+        item.saldoTotal -= dividaTotal;
+        item.compras = [];
+      }
+      return contas;
 
 }
 
