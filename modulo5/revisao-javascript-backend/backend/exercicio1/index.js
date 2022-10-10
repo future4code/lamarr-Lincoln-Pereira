3ce124ebdd452b155733e831afa984f726d01fa6
LINCOLN CARMO PEREIRA <lin.lincoln@hotmail.com>
2022-10-09 21:48:13 -0300

    inserindo exercicioos finalizados

--- modulo5/revisao-javascript-backend/backend/exercicio1/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio1/index.js
@@ -1,1 +1,17 @@
-console.log("exercicio 4");
+function converterCelsius(degree, scale) {
+    if (typeof degree !== 'number') {
+      return "O grau deve ser passado em formato de número";
+    } else {
+      if (scale === "F") {
+        const celsiusParaFahrenheit = degree * (9 / 5) + 32;
+        return `${degree}° Celsius é equivalente a ${celsiusParaFahrenheit}° Fahrenheit.`;
+      } else if (scale === "K") {
+        const celsiusParaKelvin = degree + 273.15;
+        return `${degree}° Celsius é equivalente a ${celsiusParaKelvin}° Kelvin.`;
+      } else {
+        return "Parâmetro inválido: deve passar F para converter para Fahrenheit ou K para converter para Kelvin.";
+      }
+    }
+  }
+  
+  console.log(converterCelsius(30, "F")); // output : “30° Celsius é equivalente a 86° Farenheit.”
--- modulo5/revisao-javascript-backend/backend/exercicio2/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio2/index.js
@@ -1,1 +1,45 @@
-console.log("exercício 2");
+const clientsList = [
+    { id: 1, nome: "Fulano" },
+    { id: 2, nome: "Ciclano" },
+    { id: 3, nome: "Beltrano" },
+    { id: 4, nome: "Fulana" },
+  ];
+  
+  function addClient(objeto) {
+    if (typeof objeto.id !== "number" || objeto.id < 0) {
+      return "Erro. Parâmetro inválido: passar um número real.";
+    }
+  
+    if (clientsList.some((elem) => elem.id === objeto.id)) {
+      return "Erro. Parâmetro inválido: id já existe. ";
+    }
+  
+    if (/^[A-Za-z]+$/.test(objeto.nome)) {
+      clientsList.push({ id: objeto.id, nome: objeto.nome });
+      console.log(clientsList);
+      return "Usuário(a) cadastrado(a) com sucesso!";
+    } else {
+      return "Não permite nome com caracteres especiais!";
+    }
+  }
+  
+  console.log(addClient({ id: 5, nome: "Manoela" }));
+  
+  
+  // Gerador de tabuada.
+  
+  function tabuada(multiplicador) {
+    if (multiplicador < 1 || multiplicador > 10) {
+      console.log(
+        "Erro. Parâmetro inválido (’número precisa valer entre 1 e 10’)"
+      );
+    } else if (typeof multiplicador !== "number") {
+      console.log("Erro. Parâmetro inválido (’deve ser um número’).");
+    } else {
+      for (let i = 0; i < 11; i++) {
+        console.log(`${multiplicador}x${i} = ${multiplicador * i}`);
+      }
+    }
+  }
+  
+  tabuada(5);
